8df44977d759a404fa2f9d6fa39968b3a7a82503
nerijus.sirvys <nerijus.sirvys@gmail.com>
2021-10-31 20:59:49 +0000

    Added basic identity with employee table

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public class DataContext : IdentityDbContext<Employee>
+    {
+        public DbSet<Employee> Employees { get; set; }
+        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>().ToTable("Employees");
+        }
+
+
+    }
+}
